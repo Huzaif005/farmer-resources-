db7879daf3926b7cd63adb63c1c135a21dc44057
Huzaif <huzefpatel75@gmail.com>
2026-03-11 08:27:45 +0530

    Remove GitHub Pages routing code (using Netlify _redirects)

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,5 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  // Handle GitHub Pages SPA routing
-  if (window.location.search.startsWith('?/')) {
-    const path = '/farmer-resources-' + window.location.search.slice(1);
-    window.history.replaceState(null, '', path);
-  }
-
   createRoot(document.getElementById("root")!).render(<App />);
   
